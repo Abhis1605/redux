56ee549d8189b12ddb3e3dd9b529fa9e39af5635
Abhi solanki <abhi85076@gmail.com>
2025-12-31 23:08:38 +0530

    provideing store to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+// we have to import provider from react-redux we have to install dependencies for react-redux also other wise it shows error 
+import { Provider } from 'react-redux'
+//import function for store from src/store/store.ts
+import store from './store/store'
 import './App.css'
 
 function App() {
   return (
+    // have to provide store to the app component 
+    <Provider store={store}>
     <div>
       Hello from Abhi Solanki
     </div>
+    </Provider>
   )
 }
 
