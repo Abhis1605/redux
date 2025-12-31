6d6254c59916201e57703a22528cef719f02108f
Abhi solanki <abhi85076@gmail.com>
2025-12-31 23:38:53 +0530

    fixed: app.tsx and added some material-ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-// we have to import provider from react-redux we have to install dependencies for react-redux also other wise it shows error 
-import { Provider } from 'react-redux'
+// we have to import provider from react-redux we have to install dependencies for react-redux also other wise it shows error
+import { Provider } from "react-redux";
 //import function for store from src/store/store.ts
-import store from './store/store'
-import './App.css'
+import store from "./store/store";
+import "./App.css";
+import { Container, Typography } from "@mui/material";
 
 function App() {
   return (
-    // have to provide store to the app component 
+    // have to provide store to the app component
     <Provider store={store}>
-    <div>
-      Hello from Abhi Solanki
-    </div>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h2" align="center">
+          Habit Tracker
+        </Typography>
+      </Container>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
