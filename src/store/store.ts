54b3fb08b60ef9c36257974f7a20d603a4a5dcea
Abhi solanki <abhi85076@gmail.com>
2025-12-31 23:33:26 +0530

    fixed: store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import habitsReducer from "./habit-slice"
 
-export default configureStore ({
+const store =  configureStore ({
     reducer: {
         habits: habitsReducer
     }
 })
+
+// we have to export types from our store when using typescript
+export type RootState = ReturnType <typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+// dispatch is the function which allows to tigger action's from store 
+
+export default store;
